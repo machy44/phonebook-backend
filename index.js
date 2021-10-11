ce9cca281aa5debe9b564dd748156cf025f2a7b3
alen.sljivar <alen.sljivar@five.agency>
2021-10-11 23:27:11 +0200

    finish exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const generateId = require("./utils.js").generateId;
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms  :body")
+);
 
 let persons = [
   {
